c1977cece8ec526d4fa38fec56a3a9f6c3d5d85b
metangle <zrg_dev@163.com>
2023-09-05 23:31:40 +0800

    ✨ feat: add directives

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import directives from './directives'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+for (let key of Object.keys(directives)) {
+  app.directive(key, directives[key])
+}
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <script setup>
-import { NButton } from 'naive-ui'
+import { ref } from 'vue'
+import { NButton, NInput } from 'naive-ui'
+
+let inputVal = ref('')
 </script>
 
 <template>
   <main>
     <n-button type="primary">按钮</n-button>
+    <n-input v-model="inputVal" />
+    <input type="text" v-focus />
   </main>
 </template>
 
